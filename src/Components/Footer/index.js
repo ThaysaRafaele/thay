17fa06fb2de38cfd43626104c9d7386cdbbc7b45
Thaysa Lacerda <t_thaysa.lacerda@hapvida.com.br>
2024-07-21 17:25:08 -0400

    feat: criando footer e paleta de cores

---
+++ src/Components/Footer/index.js
@@ -0,0 +1,9 @@
+import './styles.css'
+
+export function Footer() {
+    return (
+        <footer>
+            <p>&copy; 2024 Thaysa Rafaele. Todos os direitos reservados.</p>
+        </footer>
+    );
+}

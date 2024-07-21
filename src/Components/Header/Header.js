a55356ff0f4bfc617c3480670cc2e75d7ea40816
Thaysa Lacerda <t_thaysa.lacerda@hapvida.com.br>
2024-07-21 17:07:12 -0400

    feat: criando pojeto e configurando o header

---
+++ src/Components/Header/Header.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './styler.css';
+
+const Header = () => (
+  <header>
+    <h1>Thaysa Rafaele</h1>
+    <nav>
+      <ul>
+        <li><a href="#Home">Home</a></li>
+        <li><a href="#Sobre">Sobre</a></li>
+        <li><a href="#Portfolio">Portfolio</a></li>
+        <li><a href="#Contato">Contato</a></li>
+      </ul>
+    </nav>
+  </header>
+);
+
+export default Header;
